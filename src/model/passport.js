12e0830b55d104c33aea49bc027ef1bb65bd94b9
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-14 22:35:22 -0300

    implement passport model

---
+++ src/model/passport.js
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class Passport extends Model {
+  static init( connectionInstance ) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID, 
+          primaryKey: true
+        }, 
+        number: {
+          type: DataTypes.STRING( 9 ), 
+          allowNull: false, 
+          unique: true, 
+          validate: {
+            is: /^[A-Za-z0-9]{6,9}$/
+          }
+        }, 
+        expirationDate: {
+          type: DataTypes.DATEONLY, 
+          allowNull: false, 
+          validate: {
+            isDate: true
+          }, 
+          field: 'expiration_date'
+        }, 
+        countryCode: {
+          type: DataTypes.CHAR( 3 ), 
+          allowNull: false, 
+          validate: {
+            is: /\b[A-Z]{3}\b/
+          }, 
+          field: 'country_code'
+        }
+      }, 
+      {
+        connectionInstance, 
+        modelName: 'Passport', 
+        tableName: 'passport'
+      }
+    );
+  }
+}
