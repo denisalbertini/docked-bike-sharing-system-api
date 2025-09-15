fd016f210c0cf539dcee3e75513ade5ada54ecff
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-14 22:34:26 -0300

    implement model status

---
+++ src/status.js
@@ -0,0 +1,22 @@
+const bikeStatus = [
+  'NEW', 
+  'AVAILABLE', 
+  'RENTED', 
+  'MAINTENANCE_REQUESTED', 
+  'UNDER_MAINTENANCE', 
+  'RETIRED'
+];
+
+const dockStatus = [
+  'OPERATIONAL', 
+  'AVAILABLE', 
+  'OCCUPIED', 
+  'MAINTENANCE_REQUESTED', 
+  'UNDER_MAINTENANCE', 
+  'DECOMMISSIONED'
+];
+
+export {
+  bikeStatus, 
+  dockStatus
+}
