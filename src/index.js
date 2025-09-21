eb5378305b63c5d47da04260a50a787fd5922dc5
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-21 20:42:05 -0300

    implement src index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import '@dotenvx/dotenvx/config';
+import sequelize from './sequelize.js';
+import initORM from './model/index.js';
+
+try {
+  await sequelize.authenticate();
+  console.log( 'Connection has been established successfully.' );
+} catch ( error ) {
+  console.error( 'Unable to connect to the database:', error );
+}
+
+await initORM( sequelize );
+
+await sequelize.sync( { force: true } );
+
+await sequelize.close();
