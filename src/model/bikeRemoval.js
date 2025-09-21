90167e0ea2b5fb9868027224add910f72cb6ef8c
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-21 16:12:16 -0300

    implement bikeRemoval model

---
+++ src/model/bikeRemoval.js
@@ -0,0 +1,25 @@
+import BaseModel from './baseModel.js';
+import { DataTypes } from 'sequelize';
+import Bike from './bike.js';
+import Employee from './employee.js';
+
+export default class BikeRemoval extends BaseModel {
+  static modelAttributes = {
+    requestedAt: {
+      type: DataTypes.DATE, 
+      allowNull: false, 
+      defaultValue: DataTypes.NOW
+    }
+  }
+
+  static defineAssociations() {
+    this.belongsTo(
+      Bike, 
+      { foreignKey: { allowNull: false } }
+    );
+    this.belongsTo(
+      Employee, 
+      { foreignKey: { allowNull: false } }
+    );
+  }
+}
