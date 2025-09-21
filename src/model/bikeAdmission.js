599fd15fee8da9712b360d22f2ba0e1498236177
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-21 16:12:02 -0300

    implement bikeAdmission model

---
+++ src/model/bikeAdmission.js
@@ -0,0 +1,25 @@
+import BaseModel from './baseModel.js';
+import { DataTypes } from 'sequelize';
+import Bike from './bike.js';
+import Dock from './dock.js';
+
+export default class BikeAdmission extends BaseModel {
+  static modelAttributes = {
+    requestedAt: {
+      type: DataTypes.DATE, 
+      allowNull: false, 
+      defaultValue: DataTypes.NOW
+    }
+  }
+
+  static defineAssociations() {
+    this.belongsTo(
+      Bike, 
+      { foreignKey: { allowNull: false } }
+    );
+    this.belongsTo(
+      Dock, 
+      { foreignKey: { allowNull: false } }
+    );
+  }
+}
