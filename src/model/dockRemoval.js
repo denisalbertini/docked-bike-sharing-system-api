a2f002ac302bf2b40503e47f95930520f7ecda1c
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-21 16:12:49 -0300

    implement dockRemoval model

---
+++ src/model/dockRemoval.js
@@ -0,0 +1,25 @@
+import BaseModel from './baseModel.js';
+import { DataTypes } from 'sequelize';
+import Dock from './dock.js';
+import Employee from './employee.js';
+
+export default class DockRemoval extends BaseModel {
+  static modelAttributes = {
+    requestedAt: {
+      type: DataTypes.DATE, 
+      allowNull: false, 
+      defaultValue: DataTypes.NOW
+    }
+  }
+
+  static defineAssociations() {
+    this.belongsTo(
+      Dock, 
+      { foreignKey: { allowNull: false } }
+    );
+    this.belongsTo(
+      Employee, 
+      { foreignKey: { allowNull: false } }
+    );
+  }
+}
