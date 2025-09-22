9093e0050fc821e84c38be96ccf8bc80bec973ed
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-21 22:44:17 -0300

    implement sequelize base repository

---
+++ src/repository/sequelize/baseRepository.js
@@ -0,0 +1,48 @@
+import { ValidationError } from 'sequelize';
+import Result from '../../model/shared/result.js';
+
+export default class BaseRepository {
+  #model;
+  
+  constructor( model ) {
+    this.#model = model;
+  }
+  
+  async #handleOperation( operation ) {
+    try {
+      return Result.success( await operation() );
+    } catch ( error ) {
+      if ( error instanceof ValidationError )
+        return Result.failure( error.errors.map( item => item.message ) );
+      
+      return Result.failure( [ error.message ] );
+    }
+  }
+
+  findAll() {
+    return this.#handleOperation(
+      () => this.#model.findAll()
+    );
+  }
+
+  findById( id ) {
+    return this.#handleOperation(
+      () => this.#model.findByPk( id )
+    );
+  }
+
+  create() {
+    return this.#handleOperation(
+      () => this.#model.findAll()
+    );
+  }
+
+  save( modelInstance ) {
+    return this.#handleOperation(
+      async () => {
+        await modelInstance.save();
+        return null;
+      }
+    );
+  }
+}
