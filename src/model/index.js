6ec30caa07b7b976c03933a5633791a062399938
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-23 22:57:50 -0300

    update model index path const

--- src/model/index.js
+++ src/model/index.js
@@ -4,16 +4,16 @@ import { readdir } from 'fs/promises';
 
 const __filename = fileURLToPath( import.meta.url );
 const __dirname = path.dirname( __filename );
-const entitiesPath = path.join( __dirname, 'orm', 'entities' );
+const modelsPath = path.join( __dirname, 'sequelize', 'models' );
 
 export default async function init( sequelize ) {
-  const files = await readdir( entitiesPath );
+  const files = await readdir( modelsPath );
   const modules = [];
 
   await Promise.all(
     files.map(
       async file => {
-        const modulePath = path.join( entitiesPath, file );
+        const modulePath = path.join( modelsPath, file );
         const moduleUrl = pathToFileURL( modulePath ).href;
         const module = await import( moduleUrl );
         modules.push( module );
