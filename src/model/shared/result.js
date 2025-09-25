e1407d570b51491e591b03ff8105ab9e0e64cc03
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-25 15:59:03 -0300

    add error type attribute

--- src/model/shared/result.js
+++ src/model/shared/result.js
@@ -1,18 +1,19 @@
 export default class Result {
-  constructor( isSuccess, value, errors ) {
+  constructor( isSuccess, value, errors, errorType ) {
     this.isSuccess = isSuccess;
     this.value = value;
     this.errors = errors;
+    this.errorType = errorType;
 
     Object.freeze( this );
   }
 
   static success( value ) {
-    return new Result( true, value, null );
+    return new Result( true, value, null, null );
   }
 
-  static failure( errors ) {
-    return new Result( false, null, errors );
+  static failure( errors, errorType ) {
+    return new Result( false, null, errors, errorType );
   }
 
   get isFailure() {
