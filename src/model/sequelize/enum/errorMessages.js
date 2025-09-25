d2040deaf680982b4206836bf3d4a97934cacfe5
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-24 21:40:10 -0300

    implement model error messages

---
+++ src/model/sequelize/enum/errorMessages.js
@@ -0,0 +1,7 @@
+const errorMessages = {
+  baseModelConstructorMessage: "The BaseModel class cannot be instantiated.",
+  modelConstructorMessage:
+    'Do not use the "new" operator to instanciate model classes. Use the "build" static method instead.',
+};
+
+export default errorMessages;
