274a2d78225860645575bfe4e5e5a332701b7e57
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-24 23:02:28 -0300

    add method to find by serial number

--- src/repository/sequelize/repositories/dockRepository.js
+++ src/repository/sequelize/repositories/dockRepository.js
@@ -3,4 +3,10 @@ import Dock from '../../../model/sequelize/models/dock.js';
 
 export default class DockRepository extends BaseRepository {
   constructor() { super( Dock ); }
+
+  findBySerialNumber( serialNumber ) {
+    return this.handleOperation(
+      () => this.model.findOne( { where: { serialNumber } } )
+    );
+  }
 }
