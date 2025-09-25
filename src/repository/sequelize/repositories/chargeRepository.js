923ccf4dadba8b9e37bbe3802aea670a2a5873d3
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-24 23:02:16 -0300

    add method to find by null completion time

--- src/repository/sequelize/repositories/chargeRepository.js
+++ src/repository/sequelize/repositories/chargeRepository.js
@@ -3,4 +3,10 @@ import Charge from '../../../model/sequelize/models/charge.js';
 
 export default class ChargeRepository extends BaseRepository {
   constructor() { super( Charge ); }
+
+  findAllByNullCompletionTime() {
+    return this.handleOperation(
+      () => this.model.findAll( { where: { completedAt: null } } )
+    );
+  }
 }
