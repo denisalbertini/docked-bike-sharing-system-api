5622a07f49c89dbb16d141ee2d9462f88df50af1
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-24 23:01:58 -0300

    add method to find by serial number

--- src/repository/sequelize/repositories/bikeRepository.js
+++ src/repository/sequelize/repositories/bikeRepository.js
@@ -3,4 +3,10 @@ import Bike from '../../../model/sequelize/models/bike.js';
 
 export default class BikeRepository extends BaseRepository {
   constructor() { super( Bike ); }
+
+  findBySerialNumber( serialNumber ) {
+    return this.handleOperation(
+      () => this.model.findOne( { where: { serialNumber } } )
+    );
+  }
 }
