33fa3ae986f968952290af53b5a045c04f20a66a
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-24 23:03:46 -0300

    add method to find by null finish time and bike id

--- src/repository/sequelize/repositories/rentalRepository.js
+++ src/repository/sequelize/repositories/rentalRepository.js
@@ -3,4 +3,12 @@ import Rental from '../../../model/sequelize/models/rental.js';
 
 export default class RentalRepository extends BaseRepository {
   constructor() { super( Rental ); }
+
+  findByNullFinishTimeAndBikeId( bikeId ) {
+    return this.handleOperation(
+      () => this.model.findOne(
+        { where: { finishedAt: null, bikeId } }
+      )
+    );
+  }
 }
