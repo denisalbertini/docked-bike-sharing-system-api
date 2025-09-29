07151ad6192606a5200c9577027d2f2098a5e4d3
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-28 21:40:41 -0300

    implement biker service

---
+++ src/service/services/bikerService.js
@@ -0,0 +1,48 @@
+import Result from '../../model/shared/result.js';
+import { VALIDATION_ERROR } from '../../errorTypes.js';
+
+export default class BikerService {
+  #creditCardRepository;
+  #passportRepository;
+
+  constructor(
+    bikerRepository, 
+    creditCardRepository, 
+    passportRepository
+  ) {
+    super( bikerRepository );
+    this.#creditCardRepository = creditCardRepository;
+    this.#passportRepository = passportRepository;
+  }
+
+  validateCreationData(
+    {
+      foreigner, 
+      cpf, 
+      passportNumber, 
+      countryCode, 
+      password, 
+      confirmationPassword
+    }
+  ) {
+    const errors = [];
+    
+    if ( !foreigner && !cpf )
+      errors.push( 'CPF is mandatory for locals.' );
+
+    if ( foreigner && ( !passportNumber || !countryCode ) )
+      errors.push( 'Passport data is mandatory for foreigners.' );
+
+    if ( password !== confirmationPassword )
+      errors.push( 'Passwords must match.' );
+
+    if ( errors.length > 0 )
+      return Result.failure( errors, VALIDATION_ERROR );
+
+    return Result.success();
+  }
+
+  createLocal( data ) {}
+
+  createForeigner( data ) {}
+}
