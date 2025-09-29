9dde11741098b38dff2b601cf7d9bf372bcfe8b0
Denis Albertini <denis.s.albertini@gmail.com>
2025-09-28 22:32:57 -0300

    implement transaction model

---
+++ src/model/sequelize/transaction.js
@@ -0,0 +1,34 @@
+export default class SequelizeTransaction {
+  #sequelize;
+  #transaction;
+  
+  constructor( sequelize ) {
+    this.#sequelize = sequelize;
+    this.#transaction = null;
+  }
+
+  async start() {
+    if ( this.#transaction )
+      throw new Error( 'Transaction already started.' );
+
+    this.#transaction = await this.#sequelize.transaction();
+  }
+
+  async commit() {
+    if ( !this.#transaction )
+      throw new Error( 'No transaction to commit.' );
+
+    await this.#transaction.commit();
+
+    this.#transaction = null;
+  }
+
+  async rollback() {
+    if ( !this.#transaction )
+      throw new Error( 'No transaction to rollback.' );
+
+    await this.#transaction.rollback();
+
+    this.#transaction = null;
+  }
+}
