7c804b9aed03ac765753a2333d949829dd016713
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-02 20:42:13 -0300

    implement credit card service

---
+++ src/service/services/credit-card-service.js
@@ -0,0 +1,70 @@
+import BaseService from '../base-service.js';
+import Result from '../../model/shared/result.js';
+import { VALIDATION_ERROR } from '../../error-types.js';
+
+export default class CreditCardService extends BaseService {
+  validate( { number, expirationDate, cvc } ) {
+    const errors = [];
+
+    if ( !this.#validateNumber( number ) )
+      errors.push( 'Invalid credit card number.' );
+    if ( !this.#validateExpirationDate( expirationDate ) )
+      errors.push( 'Invalid credit card expiration date.' );
+    if ( !this.#validateCvc( cvc ) )
+      errors.push( 'Invalid credit card cvc.' );
+
+    if ( errors.length !== 0 )
+      return Result.failure( errors, VALIDATION_ERROR );
+
+    return Result.success();
+  }
+
+  #validateNumber( number ) {
+    const length = number.length;
+    
+    if ( length < 13 || length > 19 ) return false;
+    
+    let sum = 0;
+    let isEven = false;
+    
+    for ( let i = number.length - 1; i >= 0; i-- ) {
+      let digit = number[ i ];
+      
+      if ( isEven ) {
+        digit *= 2;
+        if ( digit > 9 ) digit -= 9;
+      }
+      
+      sum += digit;
+      isEven = !isEven;
+    }
+    
+    return sum % 10 === 0;
+  }
+
+  #validateExpirationDate( expirationDate ) {
+    const [ month, year ] = expirationDate.split( '/' ).map( Number );
+    const now = new Date();
+    const date = new Date( year, month - 1, 1 );
+    
+    return (
+      month >= 1 &&
+      month <= 12 &&
+      year >= now.getFullYear() &&
+      date >= new Date( now.getFullYear(), now.getMonth(), 1 )
+    );
+  }
+
+  #validateCvc( cvc ) {
+    return /^\d{3,4}$/.test( cvc );
+  }
+  
+  async findOrCreate( data ) {
+    const result = await this.modelRepository.findOrCreate( data );
+
+    if ( result.isFailure ) return result;
+
+    const [ creditCard ] = result.value;
+    return Result.success( creditCard );
+  }
+}
