277c96bc409104269763d7c0b9976da4269ce976
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-02 20:39:41 -0300

    add findOrCreate credit card method

--- src/repository/sequelize/repositories/credit-card-repository.js
+++ src/repository/sequelize/repositories/credit-card-repository.js
@@ -3,4 +3,10 @@ import CreditCard from '../../../model/sequelize/models/credit-card.js';
 
 export default class CreditCardRepository extends BaseRepository {
   constructor() { super( CreditCard ); }
+
+  findOrCreate( data ) {
+    return this.handleOperation(
+      () => this.model.findOrCreate( { where: data } )
+    );
+  }
 }
