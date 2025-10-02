6d81dadeade0777c7bc307969d0b922cd876c7b9
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-02 20:42:22 -0300

    implement passport service

---
+++ src/service/services/passportService.js
@@ -0,0 +1,16 @@
+import BaseService from '../base-service.js';
+import Result from '../../model/shared/result.js';
+
+export default class PassportService extends BaseService {
+  async findByBikerId( id ) {
+    const result = await this.modelRepository.findByBikerId( id );
+
+    if ( result.isSuccess && result.value === null )
+      return Result.failure(
+        [ 'No entry found.' ], 
+        NOT_FOUND_ERROR
+      );
+
+    return result;
+  }
+}
