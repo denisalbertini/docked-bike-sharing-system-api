def6bf0942ea6ae691fdadf8613944ceaf949203
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-05 20:15:49 -0300

    add find by email method to biker repo

--- src/repository/sequelize/repositories/biker-repository.js
+++ src/repository/sequelize/repositories/biker-repository.js
@@ -3,4 +3,10 @@ import Biker from '../../../model/sequelize/models/biker.js';
 
 export default class BikerRepository extends BaseRepository {
   constructor() { super( Biker ); }
+
+  findByEmail( email ) {
+    return this.handleOperation(
+      () => this.model.findOne( { where: email } )
+    );
+  }
 }
