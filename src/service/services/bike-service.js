1f79473b0c7956febd5c2f6b2ee6f4f437727755
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-05 19:27:14 -0300

    add some methods to bike service

--- src/service/services/bike-service.js
+++ src/service/services/bike-service.js
@@ -1,3 +1,39 @@
 import BaseService from '../base-service.js';
+import bikeStatus from '../../model/shared/enum/bike-status.js';
+import Result from '../../model/shared/result.js';
+import { PRECONDITION_FAILED_ERROR } from '../../error-types.js';
 
-export default class BikeService extends BaseService {}
+export default class BikeService extends BaseService {
+  async findBySerialNumber( serialNumber ) {
+    const findResult = await this.modelRepository.findBySerialNumber(
+      serialNumber
+    );
+
+    if ( findResult.isFailure && findResult.errorType === NOT_FOUND_ERROR )
+      return Result.failure( NOT_FOUND_ERROR, 'Bike not found.' );
+
+    return findResult;
+  }
+  
+  async isAvailable( id ) {
+    const findResult = await this.findById( id );
+
+    if ( findResult.isSuccess && findResult.value.status !== bikeStatus.AVAILABLE )
+      return Result.failure( PRECONDITION_FAILED_ERROR, 'Bike is not available.' );
+
+    return findResult;
+  }
+
+  async isRented( serialNumber ) {
+    const findResult = await this.findBySerialNumber( serialNumber );
+
+    if ( findResult.isSuccess && findResult.value.status !== bikeStatus.RENTED )
+      return Result.failure( PRECONDITION_FAILED_ERROR, 'Bike is not rented.' );
+
+    return findResult;
+  }
+
+  updateStatus( id, status ) {
+    return this.updateById( id, { status } );
+  }
+}
