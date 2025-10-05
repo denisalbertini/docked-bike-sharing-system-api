d683fe85c843992360415056f9c88ae7ea29ab8d
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-05 19:29:37 -0300

    add some methods to rental service

--- src/service/services/rental-service.js
+++ src/service/services/rental-service.js
@@ -1,7 +1,15 @@
 import BaseService from '../base-service.js';
 
 export default class RentalService extends BaseService {
-  findByNullFinishTimeAndBikerId( bikerId ) {
-    return this.findByNullFinishTimeAndBikerId( bikerId );
+  findUnfinishedByBikeId( bikeId ) {
+    return this.modelRepository.findByNullFinishTimeAndBikeId( bikeId );
+  }
+  
+  findUnfinishedByBikerId( bikerId ) {
+    return this.modelRepository.findByNullFinishTimeAndBikerId( bikerId );
+  }
+
+  finish( rental ) {
+    return this.updateById( rental.id, rental );
   }
 }
