9c36bca178a02465122b8016207de28f055cf9e6
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-05 22:21:23 -0300

    add find by registration method to employee repo

--- src/repository/sequelize/repositories/employee-repository.js
+++ src/repository/sequelize/repositories/employee-repository.js
@@ -3,4 +3,10 @@ import Employee from '../../../model/sequelize/models/employee.js';
 
 export default class EmployeeRepository extends BaseRepository {
   constructor() { super( Employee ); }
+
+  findByRegistration( registration ) {
+    return this.handleOperation(
+      () => this.model.findOne( { where: registration } )
+    );
+  }
 }
