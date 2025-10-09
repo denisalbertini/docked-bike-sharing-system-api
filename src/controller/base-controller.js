cacdafc3ed8067a10ea98179ecb26b53488da144
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-08 23:04:34 -0300

    implement base controller

---
+++ src/controller/base-controller.js
@@ -0,0 +1,62 @@
+import { errorStatusMap } from '../error-status-map.js';
+
+export default class BaseController {
+  constructor( modelService ) {
+    this._modelService = modelService;
+  }
+
+  async _handleOperation( operation, res, successStatus ) {
+    const result = await operation();
+
+    if ( result.isFailure ) {
+      const errorType = result.errorType;
+      const errors = result.erros;
+      
+      return res
+        .status( errorStatusMap.get( errorType ) )
+        .send( { errorType, errors } );
+    }
+
+    res.status( successStatus ).send( result.value );
+  }
+
+  listRecords( _req, res ) {
+    return this._handleOperation(
+      () => this._modelService.findAll(), 
+      res, 
+      200
+    );
+  }
+
+  getRecord( req, res ) {
+    return this._handleOperation(
+      () => this._modelService.findById( req.query.id ), 
+      res, 
+      200
+    );
+  }
+
+  createRecord( req, res ) {
+    return this._handleOperation(
+      () => this._modelService.create( req.body ), 
+      res, 
+      201
+    );
+  }
+
+  updateRecord( req, res ) {
+    return this._handleOperation(
+      () => this._modelService.updateById( req.query.id ), 
+      res,
+      200
+    );
+  }
+
+  deleteRecord( req, res ) {
+    return this._handleOperation(
+      () => this._handleOperation( req.query.id ), 
+      res, 
+      200
+    );
+  }
+}
