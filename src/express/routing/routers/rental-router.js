f69f293f23031f72701230195335d613f91aadab
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-11 22:04:47 -0300

    implement rental router

---
+++ src/express/routing/routers/rental-router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { bikerAuthMiddleware } from '../auth-middleware.js';
+import { rentalController } from '../controller-instances.js';
+
+// Router
+const router = express.Router();
+
+// Auth middlewares
+router.use( [ '/', '/return' ], bikerAuthMiddleware );
+
+// Routes
+router.route( '/' )
+  .post( rentalController.createRecord );
+
+router.route( '/return' )
+  .post( rentalController.registerReturn );
+
+export default router;
