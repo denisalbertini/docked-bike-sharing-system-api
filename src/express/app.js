498382cb47eb6cd39ca3048e64253d7662d79317
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-12 22:34:46 -0300

    add json and error handling middleware usage to app

--- src/express/app.js
+++ src/express/app.js
@@ -3,6 +3,8 @@ import router from './routing/routers/api-router.js';
 
 const app = express();
 
+app.use( express.json() );
 app.use( '/api', router );
+app.use( ( err, _req, _res, _next ) => console.error( err ) );
 
 export default app;
