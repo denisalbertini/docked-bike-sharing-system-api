386ff45d8418800b9df9b88c2e2fdc0190680e8a
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-16 22:46:56 -0300

    add merge failures method to result model

--- src/model/shared/result.js
+++ src/model/shared/result.js
@@ -1,3 +1,12 @@
+import {
+  FOREIGN_KEY_CONSTRAINT_ERROR, 
+  INTERNAL_SERVER_ERROR, 
+  NOT_FOUND_ERROR, 
+  PRECONDITION_FAILED_ERROR, 
+  UNIQUE_CONSTRAINT_ERROR, 
+  VALIDATION_ERROR
+} from './enum/error-types';
+
 const constructorErrorMessage =
   'Do not use the "new" operator to instantiate Result. Use the static "success" and "failure" methods instead.';
 
@@ -40,5 +49,25 @@ export default class Result {
       errors, 
       errorType
     );
+  }
+
+  static mergeFailures( failures ) {
+    const errorTypes = [];
+    const errors = [];
+
+    for ( const failure of failures ) {
+      errorTypes.push( failure.errorType );
+      errors.push( ...failure.errors );
+    }
+
+    const errorType = 
+      errorTypes.find( t => t === VALIDATION_ERROR ) ?? 
+      errorTypes.find( t => t === NOT_FOUND_ERROR ) ?? 
+      errorTypes.find( t => t === UNIQUE_CONSTRAINT_ERROR ) ?? 
+      errorTypes.find( t => t === FOREIGN_KEY_CONSTRAINT_ERROR ) ?? 
+      errorTypes.find( t => t === PRECONDITION_FAILED_ERROR ) ?? 
+      INTERNAL_SERVER_ERROR;
+
+    return Result.failure( errorType, ...errors );
   }
 }
