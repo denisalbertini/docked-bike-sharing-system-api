048b5230f5a02bef8827b29c6a0ff7a852fe1f9e
Denis Albertini <denis.s.albertini@gmail.com>
2025-10-19 21:28:09 -0300

    update find by email biker repo method

--- src/repository/repositories/biker-repository.js
+++ src/repository/repositories/biker-repository.js
@@ -1,12 +1,13 @@
 import Biker from '../../model/models/biker.js';
+import bikerStatus from '../../model/shared/enum/biker-status.js';
 import BaseRepository from "../base-repository.js";
 
 export default class BikerRepository extends BaseRepository {
   constructor() { super( Biker ); }
 
-  findByEmail( email ) {
+  findByEmailAndActiveStatus( email ) {
     return this._handleOperation(
-      () => this._model.findOne( { where: { email } } )
+      () => this._model.findOne( { where: { email, status: bikerStatus.ACTIVE } } )
     );
   }
 }
